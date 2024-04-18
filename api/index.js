941d3144aa59172bfbb2a935f240598a1123097e
jpkato <quanghieu.forever@gmail.com>
2024-04-18 22:47:17 +0900

    create and connect to moongodb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
+
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(() => console.log("connected to mongoose db !"))
+  .catch((err) => console.log(err));
 
 const app = express();
 
